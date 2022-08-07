258d67958d954aaba0a84ff1844a9fa3866438a1
Vibhav <vibhavsharmaha@gmail.com>
2022-08-07 11:51:00 -0700

    added data layer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import "./App.css";
 import Login from "./Login";
 import Player from "./Player";
 import { gethashfromurl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataValue } from "./DataLayer";
 const spotify = new SpotifyWebApi();
 function App() {
   const [token, settoken] = useState(null);
+  const [{ user }, dispatch] = useDataValue();
+
   useEffect(() => {
     const hash = gethashfromurl();
     window.location.hash = "";
     const _token = hash.access_token;
     if (_token) settoken(_token);
-    console.log("Token is ", _token);
+    spotify.setAccessToken(_token);
+    spotify.getMe().then((user) => {
+      dispatch({ type: "SET_USER", user: user });
+    });
+
+    // console.log("Token is ", _token);
   }, []);
+  console.log("user is ", user);
   return <>{token ? <Player /> : <Login />}</>;
 }
 
